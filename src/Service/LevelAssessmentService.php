ce5814d1d80d13164f652d72548564cdf253a1ac
Marc Guinea <develop.marcguinea@gmail.com>
2023-02-23 19:01:01 +0100

    chore: first commit

---
+++ src/Service/LevelAssessmentService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Busuu\Service;
+
+class LevelAssessmentService implements LevelAssessmentServiceInterface
+{
+    /**
+     * @param array $exercises
+     * @return string|bool
+     */
+    public function calculateLevel(array $exercises)
+    {
+        return '';
+    }
+}

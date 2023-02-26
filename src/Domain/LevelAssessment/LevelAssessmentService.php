2545f2b7ae208212655aef39970e0e6c9a1cb1c7
Marc Guinea <develop.marcguinea@gmail.com>
2023-02-26 10:04:58 +0100

    feat: implement level assessment functionality inside the service

--- src/Domain/LevelAssessment/LevelAssessmentService.php
+++ src/Domain/LevelAssessment/LevelAssessmentService.php
@@ -1,15 +1,38 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Busuu\Domain\LevelAssessment;
 
-class LevelAssessmentService implements LevelAssessmentServiceInterface
+use Busuu\Domain\Exercise\ExerciseSetCollection;
+
+final class LevelAssessmentService implements LevelAssessmentServiceInterface
 {
+    const MINIMUM_EXERCISE_SETS = 2;
+    const MAXIMUM_EXERCISE_SETS = 5;
+    const MAXIMUM_CONSECUTIVE = 2;
+
     /**
      * @param array $exercises
      * @return string|bool
      */
     public function calculateLevel(array $exercises)
     {
-        return '';
+        $levelAssessment = new LevelAssessment();
+        $exerciseSetCollection = ExerciseSetCollection::fromExercises($exercises);
+
+        if ($exerciseSetCollection->total() < self::MINIMUM_EXERCISE_SETS) {
+            return false;
+        }
+
+        $toEvaluateExerciseSetCollection = $exerciseSetCollection
+            ->slice(self::MAXIMUM_EXERCISE_SETS)
+            ->consecutive(self::MAXIMUM_CONSECUTIVE);
+
+        foreach($toEvaluateExerciseSetCollection->items() as $toEvaluateExerciseSet) {
+            $levelAssessment->applyExerciseSetPoints($toEvaluateExerciseSet->points());
+        }
+
+        return $levelAssessment->grade();
     }
 }
